fb5efcdf918989ec42ddfcc508368b1f33da44b0
Tyler Kraatz <Tmkraatz@gmail.com>
2024-09-29 17:52:50 -0400

    added relationship framework and basic title screen
    added framework for relationships and also added a very basic title screen. making sure this doesn't break everything too

---
+++ Assets/scripts/relationshipframework.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class relationshipframework : MonoBehaviour
+{
+    public float copRelations;
+    public float russianMobRelations;
+    public float norwegianMobRelations;
+    public float sicilianMobRelations;
+    public float prohibitionistsRelations;
+    public float drunkardRelations;
+    public float civilianRelations;
+}
