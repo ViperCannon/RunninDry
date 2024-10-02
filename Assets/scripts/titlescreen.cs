d0bec5c260c5ac7df7d74f2aec5107bf65305796
Tyler Kraatz <Tmkraatz@gmail.com>
2024-10-02 18:04:18 -0400

    Added credits screen and options screen to main menu

--- Assets/scripts/titlescreen.cs
+++ Assets/scripts/titlescreen.cs
@@ -6,10 +6,16 @@ using UnityEngine.SceneManagement;
 public class titlescreen : MonoBehaviour
 {
     public int sceneID;
+
+    public GameObject mainCanvas;
+    public GameObject creditsCanvas;
+    public GameObject optionsCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
+        creditsCanvas.SetActive(false);
+        optionsCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
     }
 
     // Update is called once per frame
@@ -25,5 +31,26 @@ public class titlescreen : MonoBehaviour
     public void OnQuit()
     {
         Application.Quit();
+    }
+    public void optionsOpen()
+    {
+        mainCanvas.SetActive(false);
+        optionsCanvas.SetActive(true);
+    }
+    public void optionsClose()
+    {
+        optionsCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
+    }
+
+    public void creditsOpen()
+    {
+        mainCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+    public void creditsClose()
+    {
+        creditsCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
     }
 }
