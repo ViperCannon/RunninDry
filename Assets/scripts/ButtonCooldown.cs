a09a74de85112afd8279cf4e285d0cc908ac6d8a
Michael Gary <vyperkannon@gmail.com>
2024-10-08 15:48:50 -0400

    Updated Generate Map Button
    Added a cooldown to the generate map button to disable spam clicking which would cause the program to freeze.

---
+++ Assets/scripts/ButtonCooldown.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCooldown : MonoBehaviour
+{
+    bool cooldown = false;
+    
+    [SerializeField]
+    GameObject map;
+
+    public void ButtonFunction()
+    {
+        if (!cooldown)
+        {
+            map.GetComponent<MapGenerator>().generateMap();
+            cooldown = true;
+            Invoke("ResetCooldown", 3.5f);
+        }
+    }
+
+    // Update is called once per frame
+    void ResetCooldown()
+    {
+        cooldown = false;
+    }
+}
