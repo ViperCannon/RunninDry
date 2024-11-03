7bdf715101bb98172e4ef2ca38c7be2aa007b809
Michael Gary <vyperkannon@gmail.com>
2024-11-03 07:23:53 -0500

    Combat Foundation
    Created the ground work for the combat system. Also added folders to better organize the scripts.

---
+++ Assets/scripts/Character/Enemies/EnemyData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEnemy", menuName = "Character/Enemy")]
+public class EnemyData : MonoBehaviour
+{
+    
+}
