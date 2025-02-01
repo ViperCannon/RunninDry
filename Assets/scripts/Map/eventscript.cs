39b1b9c90ca84fac86fd6d4ee1113a4b3a1b64f5
David-M-Tripp <david@tripp.net>
2025-02-01 10:13:34 -0500

    Might have messed up.
    Turns out some of my case fixing caused compilation errors and I never checked until now! ... That's a rather big oops on my part. Just fixed 'em though!

--- Assets/scripts/Map/eventscript.cs
+++ Assets/scripts/Map/eventscript.cs
@@ -17,7 +17,7 @@ public class eventscript : MonoBehaviour
     public void loadAssets()
     {
         gamemanager.talkerint = randomnumber.generatedNumber;
-        gamemanager.encounter(encounterType);
+        gamemanager.Encounter(encounterType);
         if (randomnumber.generatedNumber <= 3)
         {
             gamemanager.Eassets1.SetActive(true);
