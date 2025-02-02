2f064a3fc9e2ecc0dceb66977b4be3480053708b
David-M-Tripp <david@tripp.net>
2025-02-02 14:30:36 -0500

    More script naming convention cleanup!

--- Assets/scripts/GameManagers/pausemenu.cs
+++ Assets/scripts/GameManagers/pausemenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class pausemenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
     public int sceneID;
 
--- Assets/scripts/GameManagers/titlescreen.cs
+++ Assets/scripts/GameManagers/titlescreen.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class titlescreen : MonoBehaviour
+public class TitleScreen : MonoBehaviour
 {
     public int sceneID;
 
