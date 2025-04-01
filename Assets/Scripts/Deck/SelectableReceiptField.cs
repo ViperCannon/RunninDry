a975f15352ae54b24690496252d639b3d85667cd
David-M-Tripp <david@tripp.net>
2025-04-01 10:27:14 -0400

    Update SelectableReceiptField.cs
    Receipt fields now highlight in red when hovered over!

--- Assets/Scripts/Deck/SelectableReceiptField.cs
+++ Assets/Scripts/Deck/SelectableReceiptField.cs
@@ -5,13 +5,17 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SelectableReceiptField : MonoBehaviour, IPointerDownHandler
+public class SelectableReceiptField : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private TextMeshProUGUI receiptField;
-    
+
+    private Color32 originalColor;
+    private Color32 highlightedColor = new(163, 39, 39, 255);
+
     void Awake()
     {
         receiptField = GetComponent<TextMeshProUGUI>();
+        originalColor = receiptField.color;
     }
     
     public void OnPointerDown(PointerEventData eventData) 
@@ -21,5 +25,23 @@ public class SelectableReceiptField : MonoBehaviour, IPointerDownHandler
             return;
         }
         DeckBuilderVer2.Instance.SelectedCharacter.DeselectCard(receiptField.text);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (receiptField == null)
+        {
+            return;
+        }
+        receiptField.color = highlightedColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (receiptField == null)
+        {
+            return;
+        }
+        receiptField.color = originalColor;
     }
 }
