92a42b6299a848cde165eabc6d75c70600dc8f6f
Michael Gary <83522753+ViperCannon@users.noreply.github.com>
2025-04-14 23:03:01 -0400

    Deleted Obsolete Script
    IBuffEffect no longer needed.

--- Assets/Scripts/GameManagers/Combat/BuffsDebuffs/IBuffEffect.cs
+++
@@ -1,6 +0,0 @@
-public interface IBuffEffect
-{
-    void ResolveEffect(CombatCardDisplay cardInstance, CharacterInstance target);
-    void ResolveEffect(CombatCard action, CharacterInstance target);
-    void ResolveEffect(NegotiationCardDisplay cardInstance);
-}
